89489badbf2bb96aeb57651b71b298071597cf22
trozio <daniel.l.adelman@gmail.com>
2019-02-16 11:17:17 -0500

    routes

--- project.js
+++ project.js
@@ -40,13 +40,21 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "home.html"));
 });
 
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
 // Displays all characters
-app.get("/api/tables.html", function(req, res) {
+app.get("/api/tables", function(req, res) {
   return res.json(characters);
 });
 
 // Displays a single character, or returns false
-app.get("/api/reserve.html", function(req, res) {
+app.get("/api/reserve", function(req, res) {
   var chosen = req.params.character;
 
   console.log(chosen);
