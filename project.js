1c90b5e439eff3529fa36a6e871891b8a5385b87
trozio <daniel.l.adelman@gmail.com>
2019-02-16 11:06:14 -0500

    Update project.js

--- project.js
+++ project.js
@@ -60,6 +60,19 @@ app.get("/api/characters/:character", function(req, res) {
   return res.json(false);
 });
 
+app.post("/api/characters", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newcharacter = req.body;
+
+  console.log(newcharacter);
+
+  // We then add the json the user sent to the character array
+  characters.push(newcharacter);
+
+  // We then display the JSON to the users
+  res.json(newcharacter);
+});
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
