7c3fec98b7c9974751ef83bfb3485f117f4d0902
helmi <helmitira@yahoo.com>
2019-02-16 13:06:52 -0500

    adding post and array

--- project.js
+++ project.js
@@ -7,36 +7,31 @@ const path = require("path");
 var app = express();
 var PORT = 3000;
 
-const tables = []
-const waitlist = []
-
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var characters = [
-  {
-    routeName: "yodahgjkbhk",
-    name: "Yoda",
-    role: "Jedi Master",
-    age: 900,
-    forcePoints: 2000
-  },
-  {
-    routeName: "dart",
-    name: "Darth Maul",
-    role: "Sith Lord",
-    age: 200,
-    forcePoints: 1200
-  },
-  {
-    routeName: "obiwankenobi",
-    name: "Obi Wan Kenobi",
-    role: "Jedi Master",
-    age: 55,
-    forcePoints: 1350
-  }
-];
+let tables = [
+  {name: "Daniel",
+  phone: "1234567890",
+  email: "daniel@daniel.com",
+  customerID: 1},
+  {name: "eric",
+  phone: "1555567890",
+  email: "eric@meric.com",
+  customerID: 2},
+  {name: "sam",
+  phone: "9999999999",
+  email: "sam@sam.com",
+  customerID: 3},
+  {name: "anna",
+  phone: "1234567890",
+  email: "anna@anna.com",
+  customerID: 4},
+  {name: "samsung",
+  phone: "1555567890",
+  email: "samsung@samsung.com"
+  }];
 
 
 app.get("/", function(req, res) {
@@ -52,40 +47,68 @@ app.get("/reserve", function(req, res) {
   res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
-// Displays all characters
+// Displays all reservations in tables
 app.get("/api/tables", function(req, res) {
-  return res.json(characters);
+  return res.json(tables);
 });
 
-// Displays a single character, or returns false
-app.get("/api/reserve", function(req, res) {
-  var chosen = req.params.character;
+// // Displays a single character, or returns false
+// app.get("/api/reserve", function(req, res) {
+//   let chosen = req.params.character;
 
-  console.log(chosen);
+//   console.log(chosen);
 
-  for (var i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
-    }
+//   for (let i = 0; i < characters.length; i++) {
+//     if (chosen === characters[i].routeName) {
+//       return res.json(characters[i]);
+//     }
+//   }
+
+//   return res.json(false);
+// });
+
+// reserve API call
+app.post('/api/reserve', function (req, res) {
+	console.log('reserve request submitted');
+	console.log(req.body);
+
+  let newReservation = req.body;
+
+  tables.push(newReservation);
+
+  // Check if user is in the first 4 in list
+  let isBooked;
+  if(tables.length <= 4){
+    isBooked = true;
+  }
+  else{
+    isBooked = false;
   }
 
-  return res.json(false);
+  res.json(isBooked);
+
 });
 
-app.post("/api/characters", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newcharacter = req.body;
 
-  console.log(newcharacter);
-
-  // We then add the json the user sent to the character array
-  characters.push(newcharacter);
-
-  // We then display the JSON to the users
-  res.json(newcharacter);
+app.post('/api/clear', function (req, res) {
+  console.log('clear all tables');
+  tables = [];
+  res.sendFile(path.join(__dirname, 'app/tables.html'));
 });
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+app.post('/api/reserve', function (req, res) {
+  console.log(req.body.id);
+
+  tables.splice(req.body.id, 1);
+  res.json(tables);
 });
+
+
+app.listen(PORT, function (err) {
+  if (err) {
+    return console.error(err)
+  }
+
+  console.log(`Listening on port ${PORT}.`)
+})
+
